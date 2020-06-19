06cda5fc653b5c1be259bec0958973c978b3a6a7
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-06-18 20:56:03 -0500

    signin, Custom Input, and Custom Button components added

---
+++ medcab9/src/components/CustomInput.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./CustomInput.styles.scss";
+
+const CustomInput = ({ handleChange, label, ...otherProps }) => {
+  <input className="form-input" onChange={handleChange} {...otherProps} />;
+};
+
+export default CustomInput;
