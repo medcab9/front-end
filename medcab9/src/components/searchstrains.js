33048bfd23cef4b6a060cf8062950f195bd06058
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-06-18 22:41:46 -0500

    StrainResults and SearchStrain components added

---
+++ medcab9/src/components/searchstrains.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import CustomInput from "./CustomInput";
+import { useEffect, useState } from "react";
+
+const SearchStrains = () => {
+  const [strains, setStrains] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const strainsFilter = (e) => {
+    setResults(e.target.value);
+  };
+
+  //loading page
+  useEffect(() => {
+    axios
+      .get("https://cannedmedical.herokuapp.com/api/strain")
+      .then((res) => {
+        console.log(res.data);
+        setStrains(res.data);
+      })
+      .catch((err) => console.log("Oh oh, something went wrong: ", err));
+  }, [results]);
+
+  //search for strains
+  useEffect(() => {
+    const results = strains.filter((strain) => {
+      return strain.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
+    setResults(results);
+  }, [strains, searchTerm]);
+
+  return (
+    <CustomInput
+      type="search"
+      placeholder="Search Strains Here..."
+      strainsFilter={strainsFilter}
+    />
+    <StrainResults results={results} />
+  );
+};
+
+export default SearchStrains;
