71d11b96a5704e3247c284538445dac7ad077d35
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-06-22 19:10:19 -0500

    fixed the CustomInput component

--- medcab9/src/components/CustomInput.js
+++ medcab9/src/components/CustomInput.js
@@ -3,7 +3,11 @@ import React from "react";
 import "./CustomInput.styles.scss";
 
 const CustomInput = ({ handleChange, label, ...otherProps }) => {
-  return <input className="form-input" onChange={handleChange} {...otherProps} />;
+  return (
+    <div>
+      <input className="form-input" onChange={handleChange} {...otherProps} />;
+    </div>
+  );
 };
 
 export default CustomInput;
