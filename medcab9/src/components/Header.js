444d1f3e76a6bcbb8fa405bd500cd612d8915b83
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-06-24 00:02:46 -0500

    added styles to the strains page

--- medcab9/src/components/Header.js
+++ medcab9/src/components/Header.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-const MainHeader = styled.header`
-  margin-bottom: 20px;
-  display: flex;
-  justify-content: space-around;
-  padding: 20px;
-  width: 80%;
-`;
+import logo from "../assets/logo-transparent.png";
+import "./Header.styles.scss";
 
 const Header = () => {
   return (
-    <MainHeader>
-    <img src="/medcab9/src/assets/logo-cannMedical.jpeg" alt="logo cann medical"/>
-      <Link to="/recommendations">Recommendations</Link>
-      <Link to="/strains">Strains</Link>
-      <Link to="/">Sign In</Link>
-      <Link to="/signup">Sign Up</Link>
-    </MainHeader>
+    <nav className="uk-navbar uk-dark uk-navbar-container header" uk-navbar>
+      <div className="uk-navbar-left">
+        <img className="logo" src={logo} alt="logo cann medical" />
+      </div>
+      <div className="uk-navbar-right">
+        <ul class="uk-navbar-nav">
+          <li>
+            <Link className="link" to="/recommendations">Recommendations</Link>
+          </li>
+          <li>
+            <Link className="link" to="/strains">Strains</Link>
+          </li>
+          <li>
+            <Link className="link" to="/">Sign In</Link>
+          </li>
+          <li>
+            <Link className="link" to="/signup">Sign Up</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
