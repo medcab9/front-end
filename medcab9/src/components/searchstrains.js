9f6febd34a7386658a821def1bed4196d602f5c3
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-06-24 19:31:02 -0500

    fixed header logo, created cards to signup and signin, fixed searchbar and removed/added fields to strains

--- medcab9/src/components/searchstrains.js
+++ medcab9/src/components/searchstrains.js
@@ -5,17 +5,17 @@ import "./Searchbar.styles.scss";
 import CustomInput from "./CustomInput";
 // import StrainResults from "./StrainResults";
 
-const SearchStrains = ({ results, placeholder, handleChange,strainsFilter }) => {
+const SearchStrains = ({ results, placeholder, handleChange }) => {
   return (
     <form className="uk-search uk-search-default uk-margin-top	">
-    <span uk-search-icon></span>
+      <span uk-search-icon></span>
       <CustomInput
-      className="uk-search-input search-custom"
+        className="uk-search-input search-custom"
         type="search"
         placeholder={placeholder}
         handleChange={handleChange}
         results={results}
-        style={{border: "2px solid #8abb2a"}}
+        style={{ border: "2px solid #8abb2a" }}
       />
     </form>
   );
