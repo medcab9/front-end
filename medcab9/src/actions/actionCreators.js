b66a1026be4d612e9e66943d0454bf58f9bb851a
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-06-26 21:28:21 -0500

    added yup to the sign in

--- medcab9/src/actions/actionCreators.js
+++ medcab9/src/actions/actionCreators.js
@@ -5,7 +5,7 @@ const signinApi = "https://cannedmedical.herokuapp.com/auth/login";
 const signupApi = "https://cannedmedical.herokuapp.com/auth/register";
 
 // User Signup
-export const userSignup = (userData, history) => dispatch => {
+export const userSignup = (userData, history) => (dispatch) => {
   axiosWithAuth()
     .post(signupApi, userData)
     .then(({ data }) => {
@@ -13,12 +13,12 @@ export const userSignup = (userData, history) => dispatch => {
       localStorage.setItem("token", data.token);
       history.push("/signup");
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 //!! User Signup
 
 // User Signin && Signout
-export const userLogin = (signinData, history) => dispatch => {
+export const userLogin = (signinData, history) => (dispatch) => {
   axiosWithAuth()
     .post(signinApi, signinData)
     .then(({ data }) => {
@@ -26,50 +26,50 @@ export const userLogin = (signinData, history) => dispatch => {
       localStorage.setItem("token", data.token);
       history.push("/strains");
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
-export const signout = history => {
-  // history.push("/");
+export const signout = () => {
   localStorage.removeItem("token");
+  alert("You have signed out.");
+  const token = localStorage.getItem("token");
+  console.log("token: ", token);
+  console.log(types.SIGN_OUT);
   return { type: types.SIGN_OUT };
 };
 // User Logout
 
 //posting form for recommendations
-
-export const postRecForm = recData => dispatch => {
+export const postRecForm = (recData) => (dispatch) => {
   const stringObjRecData = JSON.stringify(recData);
   axiosWithAuth()
-    .post(
-      `https://medcab-td.herokuapp.com/predictions`,
-      { body: stringObjRecData }
-    )
+    .post(`https://medcab-td.herokuapp.com/predictions`, {
+      body: stringObjRecData,
+    })
     .then(({ data }) => {
       dispatch(displayRecList(data));
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 //!! posting form for recommendations
 
 //get  && display recommendations from DS API for recommendations page
-export const displayRecList = recommended => {
+export const displayRecList = (recommended) => {
   return { type: types.GET_RECOMMENDED, payload: recommended };
 };
 
 //post recommendation to backend and display in the ReviewList Component
-export const setReviewList = recommended => {
+export const setReviewList = (recommended) => {
   return { type: types.SAVE_RECOMMENDED, payload: recommended };
 };
 
-export const saveRecommended = recommended => dispatch => {
+export const saveRecommended = (recommended) => (dispatch) => {
   axiosWithAuth()
     .post("https://cannedmedical.herokuapp.com/saved", recommended)
     .then(({ data }) => {
       // NEED AT LEAST ID OF NEW REVIEW FROM BACKEND
       dispatch(setReviewList(recommended));
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 //!!post recommendation to backend
-
--- medcab9/src/components/signin.js
+++ medcab9/src/components/signin.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 
-import { axiosWithAuth} from "../utils/AxiosWithAuth"
+import { axiosWithAuth } from "../utils/AxiosWithAuth";
+import * as yup from "yup";
 
 import CustomInput from "./CustomInput";
 import CustomButton from "./CustomButton";
@@ -15,11 +16,31 @@ class SignIn extends React.Component {
     this.state = {
       username: "",
       password: "",
+      errors: {
+        username: "",
+        password: "",
+      },
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  passwordValidator = "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]{8,}$";
+
+  formSchema = yup.object().shape({
+    username: yup
+      .string()
+      .min(3, "Your username must have at least 3 characters.")
+      .required("Username is required."),
+    password: yup
+      .string()
+      .matches(
+        this.passwordValidator,
+        "Minimum eight characters, at least one uppercase letter, one lowercase letter and one number."
+      )
+      .required("Password is required."),
+  });
 
   handleChange(e) {
     this.setState({
@@ -35,11 +56,28 @@ class SignIn extends React.Component {
         password: this.state.password,
       })
       .then((res) => {
-        localStorage.setItem('token', res.data.token);
+        localStorage.setItem("token", res.data.token);
         this.props.history.push("/userprofile");
       })
       .catch((err) => console.log("Oh no, there is something wrong: ", err));
   }
+
+  validationOnChange = (e) => {
+    yup
+      .reach(this.formSchema, e.target.name)
+      .validate(e.target.value)
+      .then((inputIsValid) => {
+        this.setState.errors({
+          username: "",
+          password: "",
+          // [e.target.name]: "",
+        });
+      })
+      .catch((err) => {
+        this.setState.errors.username = err.errors[0];
+        this.setState.errors.passwrod = err.errors[0];
+      });
+  };
 
   render() {
     return (
@@ -57,6 +95,7 @@ class SignIn extends React.Component {
             style={{ borderBottom: "1px solid green" }}
             required
           />
+          { this.state.errors.username > 0 ? <p className="error-message">Please, type your Username</p> : null }
 
           <CustomInput
             className="uk-margin-small uk-search-input"
@@ -69,6 +108,7 @@ class SignIn extends React.Component {
             style={{ borderBottom: "1px solid green" }}
             required
           />
+          { this.state.errors.password > 0 ? <p className="error-message">Please, type your Username</p> : null }
 
           <CustomButton
             className="uk-button uk-button-default uk-margin-small"
